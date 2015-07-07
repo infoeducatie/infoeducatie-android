7c55f3c42e63f367a5826c93a9dce7bfc715c5fb
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-07 17:12:46 +0300

    Updated UI for project item

--- app/src/main/java/com/infoeducatie/app/fragments/ProjectsFragment.java
+++ app/src/main/java/com/infoeducatie/app/fragments/ProjectsFragment.java
@@ -1,19 +1,25 @@
 package com.infoeducatie.app.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.infoeducatie.app.R;
+import com.infoeducatie.app.client.entities.Project;
+import com.infoeducatie.app.helpers.AsyncTaskHelper;
+import com.infoeducatie.app.service.management.ProjectsManagement;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProjectsFragment extends Fragment {
 
+
+    private Project[] mAllProjects;
 
     public ProjectsFragment() {
         // Required empty public constructor
@@ -24,7 +30,34 @@ public class ProjectsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        loadAllProjects();
         return inflater.inflate(R.layout.fragment_projects, container, false);
+    }
+
+    private void loadAllProjects() {
+        AsyncTaskHelper.create(new AsyncTaskHelper.AsyncMethods<Project[]>() {
+            @Override
+            public Project[] doInBackground() {
+                return ProjectsManagement.getAllProjects();
+            }
+
+            @Override
+            public void onDone(Project[] value, long ms) {
+                if (value == null) {
+                    Toast.makeText(getActivity(), "EROR", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAllProjects = value;
+                String res = "";
+                for (Project project : value) {
+                    res += project.getTitle() + " ";
+                }
+                Toast.makeText(getActivity(), res, Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
     }
 
 

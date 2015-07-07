c9dbd6da593cae0fb1dc5a6d63254d6922a94875
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-07 22:53:47 +0300

    Added multimedia enum

--- app/src/main/java/com/infoeducatie/app/client/entities/ProjectCategory.java
+++ app/src/main/java/com/infoeducatie/app/client/entities/ProjectCategory.java
@@ -2,6 +2,6 @@ package com.infoeducatie.app.client.entities;
 
 import java.io.Serializable;
 
-public enum ProjectCategory implements Serializable{
-	web, utilitar, roboti, educational
+public enum ProjectCategory implements Serializable {
+    web, utilitar, roboti, educational, multimedia
 }

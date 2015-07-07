1adeae10eeeef16ff1cc79dc2ec008972b68d5eb
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-07 23:14:03 +0300

    added all enum

--- app/src/main/java/com/infoeducatie/app/client/entities/ProjectCategory.java
+++ app/src/main/java/com/infoeducatie/app/client/entities/ProjectCategory.java
@@ -3,5 +3,5 @@ package com.infoeducatie.app.client.entities;
 import java.io.Serializable;
 
 public enum ProjectCategory implements Serializable {
-    web, utilitar, roboti, educational, multimedia
+    web, utilitar, roboti, educational, multimedia, all
 }

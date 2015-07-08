7c0f59fe9e2c0400f17341d8751bda84af3c1cd5
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-08 13:06:53 +0300

    now loading the recyclerview

--- app/src/main/java/com/infoeducatie/app/fragments/NewsFragment.java
+++ app/src/main/java/com/infoeducatie/app/fragments/NewsFragment.java
@@ -3,19 +3,25 @@ package com.infoeducatie.app.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.infoeducatie.app.R;
 import com.infoeducatie.app.client.entities.News;
+import com.infoeducatie.app.recyclerviews.news.NewsAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NewsFragment extends Fragment {
 
-    private News[] news = new News[0];
+    private News[] mNews = new News[0];
+
+    private NewsAdapter mAdapter;
+    private RecyclerView mRecycler;
 
     public NewsFragment() {
         // Required empty public constructor
@@ -25,8 +31,21 @@ public class NewsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        View view = inflater.inflate(R.layout.fragment_news, container, false);
+
+        /* initialize the recycler view */
+        mRecycler = (RecyclerView) view.findViewById(R.id.fragment_news_recycler);
+        mRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new NewsAdapter();
+        mAdapter.setNews(mNews);
+        mRecycler.setAdapter(mAdapter);
+        /**/
+        refreshNews();
+        return view;
+    }
+
+    private void refreshNews() {
+
     }
 
 

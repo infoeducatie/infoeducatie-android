66d291f17ede42432673e6ccd4deb785e90e86c4
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-09 14:38:52 +0300

    loading recycler from java

--- app/src/main/java/com/infoeducatie/app/fragments/TalksFragment.java
+++ app/src/main/java/com/infoeducatie/app/fragments/TalksFragment.java
@@ -1,19 +1,30 @@
 package com.infoeducatie.app.fragments;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.infoeducatie.app.R;
+import com.infoeducatie.app.client.entities.Talk;
+import com.infoeducatie.app.recyclerviews.talks.TalksAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TalksFragment extends Fragment {
+    private Talk[] mTalks = new Talk[0];
 
+    private TalksAdapter mAdapter;
+    private RecyclerView mRecycler;
+
+    public void setTalk(Talk[] mTalks) {
+        this.mTalks = mTalks;
+    }
 
     public TalksFragment() {
         // Required empty public constructor
@@ -24,7 +35,24 @@ public class TalksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_talks, container, false);
+        View view = inflater.inflate(R.layout.fragment_talks, container, false);
+
+        /* initialize the recycler view */
+        mRecycler = (RecyclerView) view.findViewById(R.id.fragment_talks_recycler);
+        mRecycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new TalksAdapter();
+        mAdapter.setTalks(mTalks);
+        mRecycler.setAdapter(mAdapter);
+        /**/
+        if (mTalks.length == 0) {
+            /* we don't want to refresh if we already have news, on fragment create view */
+            refreshTalks();
+        }
+        return view;
+    }
+
+    private void refreshTalks() {
+
     }
 
 

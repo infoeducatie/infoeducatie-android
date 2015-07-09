7281a7436b951d7ac845aa32ffbd11d01e98c1fe
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-09 14:50:34 +0300

    switched fragment from app to support

--- app/src/main/java/com/infoeducatie/app/fragments/TalksFragment.java
+++ app/src/main/java/com/infoeducatie/app/fragments/TalksFragment.java
@@ -1,8 +1,9 @@
 package com.infoeducatie.app.fragments;
 
 
-import android.app.Fragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;

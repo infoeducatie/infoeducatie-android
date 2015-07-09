510d5caa5bb6d32c3e464f03c3c51429ebf394e5
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-09 14:42:03 +0300

    loading talks from server now and adding them to fragment

--- app/src/main/java/com/infoeducatie/app/fragments/TalksFragment.java
+++ app/src/main/java/com/infoeducatie/app/fragments/TalksFragment.java
@@ -8,10 +8,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.infoeducatie.app.R;
 import com.infoeducatie.app.client.entities.Talk;
+import com.infoeducatie.app.helpers.AsyncTaskHelper;
 import com.infoeducatie.app.recyclerviews.talks.TalksAdapter;
+import com.infoeducatie.app.service.management.TalksManagement;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,7 +25,7 @@ public class TalksFragment extends Fragment {
     private TalksAdapter mAdapter;
     private RecyclerView mRecycler;
 
-    public void setTalk(Talk[] mTalks) {
+    public void setTalks(Talk[] mTalks) {
         this.mTalks = mTalks;
     }
 
@@ -52,7 +55,26 @@ public class TalksFragment extends Fragment {
     }
 
     private void refreshTalks() {
+        AsyncTaskHelper.create(new AsyncTaskHelper.AsyncMethods<Talk[]>() {
+            @Override
+            public Talk[] doInBackground() {
+                return TalksManagement.getAllTalks();
+            }
 
+            @Override
+            public void onDone(Talk[] value, long ms) {
+                gotTalks(value);
+            }
+        });
+    }
+
+    private void gotTalks(Talk[] value) {
+        if (value == null) {
+            Toast.makeText(getActivity(), getActivity().getString(R.string.msg_con_error), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mTalks = value;
+        mAdapter.setTalks(mTalks);
     }
 
 

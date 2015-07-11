51d3c412e0859d7f17ad6b36a776e996bc5dde64
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-11 08:27:27 +0300

    added date parsing support

---
+++ app/src/main/java/com/infoeducatie/app/helpers/DateHelper.java
@@ -0,0 +1,22 @@
+package com.infoeducatie.app.helpers;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ * Created by Browsing on 7/11/2015.
+ */
+public class DateHelper {
+    /* converts a iso time stamp to a simpler string  */
+    public static String ISOFormatToString(String iso) {
+        try {
+            Calendar calendar = ISO8601.toCalendar(iso);
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+            return format.format(calendar.getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}

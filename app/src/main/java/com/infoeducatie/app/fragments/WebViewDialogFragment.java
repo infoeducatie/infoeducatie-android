ffe029396698a1b71de4b6c1f5a7261c6b982ca7
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-11 08:57:26 +0300

    added WebViewDialogFragment

---
+++ app/src/main/java/com/infoeducatie/app/fragments/WebViewDialogFragment.java
@@ -0,0 +1,48 @@
+package com.infoeducatie.app.fragments;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.infoeducatie.app.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WebViewDialogFragment extends DialogFragment {
+
+    private WebView mWebView;
+    private String html;
+
+    public WebViewDialogFragment setHtml(String html) {
+        this.html = html;
+        if (mWebView != null) {
+            mWebView.loadData(html, "text/html", "UTF-8");
+        }
+        return this;
+    }
+
+    public WebViewDialogFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_web_view_dialog, container, false);
+        mWebView = (WebView) view.findViewById(R.id.fragment_web_view_dialog_web_view);
+        if (html != null) {
+            mWebView.loadData(html, "text/html", "UTF-8");
+        }
+        return view;
+    }
+
+
+}

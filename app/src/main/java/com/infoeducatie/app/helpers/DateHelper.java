220cd442ba70af78b83f40bb6bfcebf0fb62fea3
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-11 08:34:05 +0300

    checking if string is null before

--- app/src/main/java/com/infoeducatie/app/helpers/DateHelper.java
+++ app/src/main/java/com/infoeducatie/app/helpers/DateHelper.java
@@ -10,6 +10,7 @@ import java.util.Calendar;
 public class DateHelper {
     /* converts a iso time stamp to a simpler string  */
     public static String ISOFormatToString(String iso) {
+        if (iso == null) return null;
         try {
             Calendar calendar = ISO8601.toCalendar(iso);
             SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm");

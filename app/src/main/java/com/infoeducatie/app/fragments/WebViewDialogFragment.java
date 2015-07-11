b2786523bfa37f8490090c78e056a79d5b4f387a
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-11 08:59:00 +0300

    removed fragment title

--- app/src/main/java/com/infoeducatie/app/fragments/WebViewDialogFragment.java
+++ app/src/main/java/com/infoeducatie/app/fragments/WebViewDialogFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.webkit.WebView;
 
 import com.infoeducatie.app.R;
@@ -36,6 +37,7 @@ public class WebViewDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         View view = inflater.inflate(R.layout.fragment_web_view_dialog, container, false);
         mWebView = (WebView) view.findViewById(R.id.fragment_web_view_dialog_web_view);
         if (html != null) {

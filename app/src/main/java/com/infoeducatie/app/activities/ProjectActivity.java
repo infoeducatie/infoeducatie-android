ac9b9e30d720072329b209a5d90052bb3f2e69d4
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-25 15:23:20 +0300

    loading fragment from activity

--- app/src/main/java/com/infoeducatie/app/activities/ProjectActivity.java
+++ app/src/main/java/com/infoeducatie/app/activities/ProjectActivity.java
@@ -1,18 +1,20 @@
 package com.infoeducatie.app.activities;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
 import com.infoeducatie.app.R;
+import com.infoeducatie.app.fragments.ProjectFragment;
 
 public class ProjectActivity extends ActionBarActivity {
+
+    private ProjectFragment mProjectFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
+        mProjectFragment = (ProjectFragment) getSupportFragmentManager().findFragmentById(R.id.activity_project_fragment);
     }
 
 

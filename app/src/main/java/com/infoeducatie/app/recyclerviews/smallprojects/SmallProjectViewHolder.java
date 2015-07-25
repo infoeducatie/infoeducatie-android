01eee4c370269d76be3298e07bd2e95c782b96a6
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-25 15:47:37 +0300

    added root view and click listener for item

--- app/src/main/java/com/infoeducatie/app/recyclerviews/smallprojects/SmallProjectViewHolder.java
+++ app/src/main/java/com/infoeducatie/app/recyclerviews/smallprojects/SmallProjectViewHolder.java
@@ -7,9 +7,6 @@ import android.widget.TextView;
 
 import com.infoeducatie.app.R;
 import com.infoeducatie.app.helpers.FontHelper;
-import com.infoeducatie.app.ui.CircularImageView;
-import com.infoeducatie.app.ui.RoundedImageView;
-import com.squareup.picasso.Picasso;
 
 /**
  * Created by Browsing on 7/7/2015.
@@ -19,6 +16,7 @@ public class SmallProjectViewHolder extends RecyclerView.ViewHolder {
     public TextView title;
     public TextView participants;
     public TextView countycategory;
+    public View root;
 
     public SmallProjectViewHolder(View itemView) {
         super(itemView);
@@ -26,6 +24,7 @@ public class SmallProjectViewHolder extends RecyclerView.ViewHolder {
         title = (TextView) itemView.findViewById(R.id.item_project_small_title);
         participants = (TextView) itemView.findViewById(R.id.item_project_small_participants);
         countycategory = (TextView) itemView.findViewById(R.id.item_project_small_city_category);
+        root = itemView.findViewById(R.id.item_project_small_root);
         /* set the typefaces */
         title.setTypeface(FontHelper.LATO_REGULAR);
         participants.setTypeface(FontHelper.LATO_REGULAR);

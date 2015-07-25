d28669c1fc6014c50f60e9f8210ba1de11714d0d
Boldijar Paul <paul.bv96@yahoo.com>
2015-07-25 15:43:37 +0300

    loading the project by id inside the activity

--- app/src/main/java/com/infoeducatie/app/activities/ProjectActivity.java
+++ app/src/main/java/com/infoeducatie/app/activities/ProjectActivity.java
@@ -4,14 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 import com.infoeducatie.app.R;
-import com.infoeducatie.app.client.entities.Contestant;
 import com.infoeducatie.app.client.entities.Project;
-import com.infoeducatie.app.client.entities.ProjectCategory;
-import com.infoeducatie.app.client.entities.Screenshot;
 import com.infoeducatie.app.fragments.ProjectFragment;
+import com.infoeducatie.app.helpers.AsyncTaskHelper;
+import com.infoeducatie.app.helpers.UIMessageHelper;
+import com.infoeducatie.app.service.management.ProjectsManagement;
 
 public class ProjectActivity extends ActionBarActivity {
 
+    /* this activity must have a id for the project to load with the name 'id' inside the intent*/
     private ProjectFragment mProjectFragment;
 
     @Override
@@ -20,6 +21,30 @@ public class ProjectActivity extends ActionBarActivity {
         setContentView(R.layout.activity_project);
         mProjectFragment = (ProjectFragment) getSupportFragmentManager().findFragmentById(R.id.activity_project_fragment);
 
+
+        final int projectId = getIntent().getIntExtra("id", -1);
+        if (projectId == -1) {
+            /* no project id */
+            finish();
+            return;
+        }
+        /* load the project */
+        AsyncTaskHelper.create(new AsyncTaskHelper.AsyncMethods<Project>() {
+            @Override
+            public Project doInBackground() {
+                return ProjectsManagement.getProject(projectId);
+            }
+
+            @Override
+            public void onDone(Project project, long ms) {
+                if (project == null) {
+                    UIMessageHelper.showNetworkErrorMessage(ProjectActivity.this);
+                } else {
+                    /* we got the project! */
+                    mProjectFragment.setProject(project);
+                }
+            }
+        });
 
     }
 
